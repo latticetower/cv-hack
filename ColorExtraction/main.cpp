4ec77469a065cb73d3b1e8ac9248c53a1e82a01d
Eugene Malygin <evgeniy.malygin@divisionlabs.com>
2017-10-07 23:31:36 +0300

    осветление кадра и фильтрация мелких и крупных компонент

--- ColorExtraction/main.cpp
+++ ColorExtraction/main.cpp
@@ -2,8 +2,8 @@
 #include <opencv2/opencv.hpp>
 
 inline bool isGreaterWithBounds(int color1, int color2){
-    const int lowerThr = 40;
-    const int upperThr = 130;
+    const int lowerThr = 50;
+    const int upperThr = 190;
     return (color1 > color2 + lowerThr) && (color1 < color2 + upperThr);
 }
 
@@ -42,6 +42,56 @@ cv::Mat findBlueRed(const cv::Mat &frame){
     return result;
 }
 
+
+cv::Mat increaseBrightness(const cv::Mat frame){
+    cv::Mat hsv;
+    cv::cvtColor(frame, hsv, CV_BGR2HSV);
+    for (int y = 0; y < hsv.rows; y++){
+        for (int x = 0; x < hsv.cols; x++){
+            if (hsv.at<uchar>(y, 3*x + 1) > 60){
+                int value = hsv.at<uchar>(y, 3*x + 2);
+                value = (value + 10)*2.5;
+                value = std::min(value, (int)255);
+                hsv.at<uchar>(y, 3*x + 2) = value;
+            }
+        }
+    }
+    cv::Mat result;
+    cv::cvtColor(hsv, result, CV_HSV2BGR);
+    return result;
+}
+
+cv::Mat filterBigComponents(const cv::Mat &br){
+    const int upperSizeThr = 40;
+    const int lowerSizeThr = 3;
+    cv::Mat mask = cv::Mat::zeros(br.size(), CV_8UC1);
+    for (int y = 0; y < br.rows; y++){
+        for (int x = 0; x < br.cols; x++){
+            const uchar *p = &br.at<uchar>(y, 3*x);
+            if ((int)p[0] + (int)p[1] + (int)p[2] > 0){
+                mask.at<uchar>(y, x) = 2;
+            }
+        }
+    }
+    for (int y = 0; y < mask.rows; y++){
+        for (int x = 0; x < mask.cols; x++){
+            if (mask.at<uchar>(y, x) != 2){
+                continue;
+            }
+            cv::Rect roi;
+            cv::floodFill(mask, cv::Point2i(x, y), cv::Scalar::all(1), &roi);
+            if (roi.width > upperSizeThr || roi.height > upperSizeThr || roi.width < lowerSizeThr|| roi.height < lowerSizeThr){
+                cv::floodFill(mask, cv::Point2i(x, y), cv::Scalar::all(0));
+            }
+        }
+    }
+	
+    cv::Mat result;
+    br.copyTo(result, mask);
+   
+    return result;
+}
+
 int main(int argc, char **argv) {
     std::string imagesIn = "/media/mes/f9289561-58c2-46ea-9c7e-42c8e63fb3da/rtsd-d1-frames/train/";
     
@@ -52,14 +102,20 @@ int main(int argc, char **argv) {
     for (const std::string &path : imgpaths){
 	cv::Mat frame = cv::imread("/" + path, CV_LOAD_IMAGE_COLOR);
         std::cout << path << std::endl;
+        
         cv::Mat blurred;
-        cv::blur(frame, blurred, cv::Size(5,5));
-        cv::Mat br = findBlueRed(blurred);
-        cv::imshow("frame", br);
-        cv::waitKey(100);
+        cv::blur(frame, blurred, cv::Size(3,3));
+        
+        cv::Mat bright = increaseBrightness(blurred);
+
+        cv::Mat br = findBlueRed(bright);
+
+        cv::Mat filtered = filterBigComponents(br);
 
         cv::Mat vis;
-        cv::vconcat(frame, br, vis);
+        cv::vconcat(frame, bright, vis);
+        cv::vconcat(vis, br, vis);
+        cv::vconcat(vis, filtered, vis);
 	cv::imwrite("output/" + std::to_string(count++)+".png", vis);
     }
 
